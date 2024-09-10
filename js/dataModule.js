aac6568dc9276c6ec5197e38a8fa868773fc748b
HusseinElAshry <hussienelashry93@gmail.com>
2024-09-10 13:06:01 +0300

    update v-2.2

--- js/dataModule.js
+++ js/dataModule.js
@@ -3,7 +3,7 @@ let baseUrl = 'https://github.com/HusseinElAshry/countries/blob/main/';
 const fetchData = async(e)=>{
     const response = await fetch(`${baseUrl}data.json`,{
         headers:{
-            'Access-Control-Allow-Origin':'[http://127.0.0.1:3800/,https://husseinelashry.github.io/countries/]'
+            'Access-Control-Allow-Origin':'https://husseinelashry.github.io/*'
         }
     });
     data = await response.json();
